5f81026755f6dbd760a5e6bbaa80276d3a868aa6
david <simplesam8856@gmail.com>
2021-09-27 14:10:03 +1000

    inital

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from 'react'
+
+import { Tween, Power3 } from 'gsap/gsap-core'
+import { gsap } from 'gsap'
+
+import './App.css'
 
 function App() {
+  let app = useRef(null)
+  let circle = useRef(null)
+  let circleRed = useRef(null)
+  let circleBlue = useRef(null)
+
+  useEffect(() => {
+    gsap.to(app, { duration: 0, css: { visibility: 'visible' } })
+    // gsap.from(circle, {
+    //   opacity: 0,
+    //   x: 40,
+    //   ease: Power3.easeOut,
+    //   duration: 0.8,
+    // })
+    // gsap.from(circleRed, {
+    //   opacity: 0,
+    //   x: 40,
+    //   ease: Power3.easeOut,
+    //   duration: 0.8,
+    //   delay: 0.2,
+    // })
+    // gsap.from(circleBlue, {
+    //   opacity: 0,
+    //   x: 40,
+    //   ease: Power3.easeOut,
+    //   duration: 0.8,
+    //   delay: 0.4,
+    // })
+
+    gsap.from('.circle', {
+      opacity: 0,
+      x: 40,
+      ease: Power3.easeOut,
+      duration: 0.8,
+      stagger: {
+        amount: 0.8,
+      },
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div ref={(el) => (app = el)} className='App'>
+      <header className='App-header'>
+        <div className='circle-container'>
+          <div ref={(el) => (circle = el)} className='circle'></div>
+          <div ref={(el) => (circleRed = el)} className='circle red'></div>
+          <div ref={(el) => (circleBlue = el)} className='circle blue'></div>
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
